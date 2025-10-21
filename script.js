4bbd90cff024f2f006b1a4ed78878466710b3335
pedrin08-rgb <reginaldopedro4444@gmail.com>
2025-10-21 10:54:26 -0300

    add cursor

--- script.js
+++ script.js
@@ -311,3 +311,18 @@ function updateSVGColors() {
 themeToggle.addEventListener('change', updateSVGColors);
 
 window.addEventListener('load', updateSVGColors);
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.clientY + "px";
+  cursor.style.left = e.clientX + "px";
+});
+
+// Efeito suave de movimento
+document.addEventListener("mousedown", () => {
+  cursor.style.transform = "translate(-50%, -50%) scale(0.8)";
+});
+document.addEventListener("mouseup", () => {
+  cursor.style.transform = "translate(-50%, -50%) scale(1)";
+});
